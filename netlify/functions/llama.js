10dbf40ab334cfd11e9ce564b9b5323ebbf7d4d6
shainemaglapuz <sjmaglapuz@gmail.com>
2025-07-16 00:31:36 +0800

    Update llama.js

--- netlify/functions/llama.js
+++ netlify/functions/llama.js
@@ -22,7 +22,7 @@ const handler = async (event, context) => {
       };
     }
 
-   const res = await fetch("https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta", {
+const res = await fetch("https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta", {
   method: "POST",
   headers: {
     Authorization: `Bearer ${HF_API_KEY}`,
@@ -36,6 +36,7 @@ const handler = async (event, context) => {
     }
   })
 });
+
 
     const rawText = await res.text();
 
